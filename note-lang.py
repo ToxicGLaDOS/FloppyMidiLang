14dd160ab226bc2c6743b9e6cdae38abc640e46f
Jeff Smith <smit8397@d.umn.edu>
2018-03-16 14:41:35 -0500

    Added support for playing chords and fixed some bugs in the main regex.

--- note-lang.py
+++ note-lang.py
@@ -43,36 +43,51 @@ with open(readFile, "r") as read:
 
 
         # Capture description:
-        # 0: Any single letter a-g either capital or lowercase
-        # 1: Sharp or flat (optional)
-        # 2: Any single number 0-9
-        # 3: At least one number from 0-9 (optional)
-        # 4: Dot followed by any number of numbers (optional)
-        # 5: Any amount of whitespace (optional)
+        # 0: All notes including whitespace between and after (at least one required)
+        # 1: The last note matched (useless)
+        # 2: Any single letter a-g either capital or lowercase
+        # 3: Sharp or flat (optional)
+        # 4: Any single number 0-9
+        # 5: At least one number from 0-9 (optional)
+        # 6: Dot followed by any number of numbers (optional)
+        # 7: Any amount of whitespace (optional)
         # Examples: 
         # c4 1000 \n\n
         # b1        \n \n               \n
         # DOES NOT SUPPORT NEGATIVE OCTAVES
-        notes = re.findall(r'([A-Ga-g])([#|b])?([0-9])\s*([0-9]+)?(.[0-9]*)?(\s*)?', data)
+        noteData = re.findall(r'((([A-Ga-g])([#|b])?([0-9])\s+)+)([0-9]+)?(.[0-9]*)?(\s*)?', data)
         write.write("dev " + "\"" + deviceName + "\"\n")
-        for note in notes:
-            letter = note[0]
-            shift = 0
-            if note[1] == "#":
-                shift = 1
-            elif note[1] == "b":
-                shift = -1
+        for note_data in noteData:
+            print note_data
+            notes = note_data[0].split()
+            notes = [note.strip() for note in notes]
+            for note in notes:
+                letter = note[0]
+                shift = 0
+                if note[1] == "#":
+                    shift = 1
+                elif note[1] == "b":
+                    shift = -1
 
-            octave = int(note[2])
-            # Turnary operator in python
-            # if duration it's the duration time the length of a quarter note
-            # otherwise it's just quarterNoteLength
-            duration = quarterNoteLength * float(note[3] + note[4]) if note[3] else quarterNoteLength
-            write.write("on " + str(midiNumber(letter, octave, shift)) + " 127\n")
+                octave = int(note[-1])
+                # Turnary operator in python
+                # if duration it's the duration time the length of a quarter note
+                # otherwise it's just quarterNoteLength
+                duration = quarterNoteLength * float(note_data[4] + note_data[5]) if note_data[4] else quarterNoteLength
+                write.write("on " + str(midiNumber(letter, octave, shift)) + " 127\n")
             write.write(formatTime(duration)+"\n")
-            write.write("off " + str(midiNumber(letter, octave, shift)) + " 127\n")
+            for note in notes:
+                letter = note[0]
+                shift = 0
+                if note[1] == "#":
+                    shift = 1
+                elif note[1] == "b":
+                    shift = -1
+
+                octave = int(note[-1])
+                write.write("off " + str(midiNumber(letter, octave, shift)) + " 127\n")
             write.write(formatTime(notePadding)+"\n")
-            
+        
 
 
 
