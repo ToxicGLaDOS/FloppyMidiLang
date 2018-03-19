0cc4530646c3b62ae3090193b75d6a20379f7a74
Jeff Smith <smit8397@d.umn.edu>
2018-03-19 15:04:12 -0500

    Fixed a bug where notes without a quarter note mulitplier would always be inturpretted as a chord. Also added rests.

--- note-lang.py
+++ note-lang.py
@@ -43,50 +43,68 @@ with open(readFile, "r") as read:
 
 
         # Capture description:
-        # 0: All notes including whitespace between and after (at least one required)
-        # 1: The last note matched (useless)
-        # 2: Any single letter a-g either capital or lowercase
-        # 3: Sharp or flat (optional)
-        # 4: Any single number 0-9
-        # 5: At least one number from 0-9 (optional)
-        # 6: Dot followed by any number of numbers (optional)
-        # 7: Any amount of whitespace (optional)
+        # 0: The whole line. This decides wether it's a rest or not
+        # 1: All notes including whitespace between and after
+        # 2: The last note matched (useless)
+        # 3: The last letter matched (useless)
+        # 4: Sharp or flat (optional)
+        # 5: Any single number 0-9
+        # 6: At least one number from 0-9 (optional)
+        # 7: Dot followed by any number of numbers (optional)
+        # 8: Any amount of whitespace (optional)
+        # Comments:
+        # [ \t\r\f\v] is most whitespace besides newline
         # Examples: 
         # c4 1000 \n\n
         # b1        \n \n               \n
         # DOES NOT SUPPORT NEGATIVE OCTAVES
-        noteData = re.findall(r'((([A-Ga-g])([#|b])?([0-9])\s+)+)([0-9]+)?(.[0-9]*)?(\s*)?', data)
+        allNotesData = re.findall(r'((([A-Ga-g][#|b]?[0-9]([ \t\r\f\v])*)+)([0-9]+)?(.[0-9]*)?([ \t\r\f\v]*)|rest[ \t\r\f\v]*(\d)*)', data)
         write.write("dev " + "\"" + deviceName + "\"\n")
-        for note_data in noteData:
-            notes = note_data[0].split()
-            notes = [note.strip() for note in notes]
-            for note in notes:
-                letter = note[0]
-                shift = 0
-                if note[1] == "#":
-                    shift = 1
-                elif note[1] == "b":
-                    shift = -1
-
-                octave = int(note[-1])
-                # Turnary operator in python
-                # if duration it's the duration time the length of a quarter note
-                # otherwise it's just quarterNoteLength
-                duration = quarterNoteLength * float(note_data[4] + note_data[5]) if note_data[4] else quarterNoteLength
-                write.write("on " + str(midiNumber(letter, octave, shift)) + " 127\n")
-            write.write(formatTime(duration)+"\n")
-            for note in notes:
-                letter = note[0]
-                shift = 0
-                if note[1] == "#":
-                    shift = 1
-                elif note[1] == "b":
-                    shift = -1
-
-                octave = int(note[-1])
-                write.write("off " + str(midiNumber(letter, octave, shift)) + " 127\n")
-            write.write(formatTime(notePadding)+"\n")
         
+        for note_data in allNotesData:
+            if "rest" in note_data[0]:
+                print "rest"
+                duration = note_data[-1]
+                duration = float(duration) if duration else 1
+                write.write(formatTime(duration * quarterNoteLength)+"\n")
+            else:
+                notes = note_data[1].split()
+                notes = [note.strip() for note in notes]
+                wholeNumber = note_data[4]
+                wholeNumber = float(wholeNumber) if wholeNumber else 0
+
+                decimalNumber = note_data[5]
+                # Cast to float or set to 0 if isn't a number
+                decimalNumber = float(decimalNumber) if decimalNumber else 0
+                # Write note on stuff
+                for note in notes:
+                    letter = note[0]
+                    shift = 0
+                    if note[1] == "#":
+                        shift = 1
+                    elif note[1] == "b":
+                        shift = -1
+
+                    octave = int(note[-1])
+                    # Turnary operator in python
+                    # if duration it's the duration time the length of a quarter note
+                    # otherwise it's just quarterNoteLength
+                    duration = quarterNoteLength * wholeNumber + decimalNumber if decimalNumber or wholeNumber else quarterNoteLength
+                    write.write("on " + str(midiNumber(letter, octave, shift)) + " 127\n")
+                write.write(formatTime(duration)+"\n")
+                # Write note off stuff
+                for note in notes:
+                    letter = note[0]
+                    shift = 0
+                    if note[1] == "#":
+                        shift = 1
+                    elif note[1] == "b":
+                        shift = -1
+                    octave = int(note[-1])
+
+                    write.write("off " + str(midiNumber(letter, octave, shift)) + " 127\n")
+                write.write(formatTime(notePadding)+"\n")
+            
 
 
 
